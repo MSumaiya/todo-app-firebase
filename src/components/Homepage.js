6bd6ab439a93ce12d7a360b953a1a4c526bcfe5f
MSumaiya <mirza01.alam@gmail.com>
2022-02-22 12:47:15 +0100

    delete and update done

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,18 +1,35 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { signOut, onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase.js";
+import { auth, db } from "../firebase.js";
 import { useNavigate } from "react-router-dom";
+import { uid } from "uid";
+import { set, ref, onValue, remove, update } from "firebase/database";
+
 export default function Homepage() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [tempUidd, setTempUidd] = useState("");
   const navigate = useNavigate();
 
-useEffect(()=>{
- auth.onAuthStateChanged(user=>{
-     if(!user){
-         navigate('/')
-     }
- })
-
-},[])
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        // read
+        onValue(ref(db, `/${auth.currentUser.uid}`), (snapshot) => {
+          setTodos([]);
+          const data = snapshot.val();
+          if (data !== null) {
+            Object.values(data).map((todo) => {
+              setTodos((oldArray) => [...oldArray, todo]);
+            });
+          }
+        });
+      } else if (!user) {
+        navigate("/");
+      }
+    });
+  }, []);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -23,9 +40,62 @@ useEffect(()=>{
         alert(err.message);
       });
   };
+
+  //add
+  const writeToDatabase = () => {
+    const uidd = uid();
+    set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+      todo: todo,
+      uidd: uidd,
+    });
+    setTodo("");
+  };
+  //update
+
+  const handleUpdate = (todo) => {
+    setIsEdit(true);
+    setTodo(todo.todo);
+    setTempUidd(todo.uidd);
+  };
+
+  const handleEditConfirm = () => {
+    update(ref(db, `/${auth.currentUser.uid}/${tempUidd}`), {
+      todo: todo,
+      tempUidd: tempUidd
+    });
+
+    setTodo("");
+    setIsEdit(false);
+  };
+  //delete
+
+  const handleDelete = (uid) => {
+    remove(ref(db, `/${auth.currentUser.uid}/${uid}`));
+  };
   return (
     <div>
-      <h1>test</h1>
+      <input
+        type="text"
+        placeholder="enter what to do..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      {todos.map((todo) => (
+        <div  key={todo.uidd}>
+          <h1>{todo.todo}</h1>
+          <button onClick={() => handleUpdate(todo)}>update</button>
+          <button onClick={() => handleDelete(todo.uidd)}>delete</button>
+        </div>
+      ))}
+      {isEdit ? (
+        <div>
+          <button onClick={handleEditConfirm}> confirm </button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={writeToDatabase}>add</button>
+        </div>
+      )}
       <button onClick={handleSignOut}>signout</button>
     </div>
   );
